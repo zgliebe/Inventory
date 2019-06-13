257abdb9fa32b5f7967f286a8399f49e8ff50edb
zgliebe <51678168+zgliebe@users.noreply.github.com>
2019-06-12 21:37:51 -0700

    Update Inventory.rb

--- Inventory.rb
+++ Inventory.rb
@@ -4,9 +4,9 @@ require 'rubyXL/convenience_methods/color'
 require 'rubyXL/convenience_methods/font'
 require 'rubyXL/convenience_methods/workbook'
 require 'rubyXL/convenience_methods/worksheet'
-workbook = RubyXL::Parser.parse("C:\Users\Red_Beard\Desktop\Coding\Inventory compiler\Test_Inventory.xlsx")
+workbook = RubyXL::Parser.parse("")
 #workbook = RubyXL::Workbook.new
 #workbook['Sheet1']
 
 
-#C:\Users\Red Beard\Desktop\Coding\Inventory compiler.xlsx
+#
