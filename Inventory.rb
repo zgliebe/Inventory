17f245c98213126b3fddd63185c353bb5d183a43
zgliebe <51678168+zgliebe@users.noreply.github.com>
2019-06-12 21:37:13 -0700

    Add files via upload

---
+++ Inventory.rb
@@ -0,0 +1,12 @@
+require 'rubyXL'
+require 'rubyXL/convenience_methods/cell'
+require 'rubyXL/convenience_methods/color'
+require 'rubyXL/convenience_methods/font'
+require 'rubyXL/convenience_methods/workbook'
+require 'rubyXL/convenience_methods/worksheet'
+workbook = RubyXL::Parser.parse("C:\Users\Red_Beard\Desktop\Coding\Inventory compiler\Test_Inventory.xlsx")
+#workbook = RubyXL::Workbook.new
+#workbook['Sheet1']
+
+
+#C:\Users\Red Beard\Desktop\Coding\Inventory compiler.xlsx
